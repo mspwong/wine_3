4cf60ef17846eea56308ee55f9612442add4199e
Mary Wong <mspwong@hotmail.com>
2011-08-19 16:02:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine3::Application.config.secret_token = '0888826049de5d95d7851623b3c910acefe030d4e08fe6029c50630be9d19d69801c7e0901e799942118e686b3f6bf40f73aa8e623afe6a81e52dcc150551e84'
