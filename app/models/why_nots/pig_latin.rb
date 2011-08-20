6c932de393ecda44470c8af70b72004df179d697
Mary Wong <mspwong@hotmail.com>
2011-08-19 17:10:02 -0700

    pig latin converted to ruby 1.9.2 to pass 1st unit test.

--- app/models/why_nots/pig_latin.rb
+++ app/models/why_nots/pig_latin.rb
@@ -1,15 +1,17 @@
 class WhyNots::PigLatin
 
   def self.translate(text)
-    text.split(" ").collect do |word|
-      chars = word.downcase.gsub(/[^a-z ]/, '').gsub(/ /, '-').chars
+    foo = text.split(" ").inject("") do |memo, word|
+      chars = word.chars
+      #chars = word.downcase.gsub(/[^a-z ]/, '').gsub(/ /, '-').chars
       pig_latin_ending = "ay "
-      pig_latin_word = (chars.to_a[1, word.length] << chars.first << pig_latin_ending).to_s
-      pig_latin_word.capitalize! if word.first == word.first.capitalize
-      punctuations = word.chars.to_a[pig_latin_word.length-pig_latin_ending.length, word.length].to_s
-      pig_latin_word.chop! << punctuations unless punctuations.blank?
-      pig_latin_word
-    end.to_s.strip
+      pig_latin_word = (word[1, word.length] << word.first << pig_latin_ending)
+      #pig_latin_word.capitalize! if word.first == word.first.capitalize
+      #punctuations = word.chars.to_a[pig_latin_word.length-pig_latin_ending.length, word.length].to_s
+      #pig_latin_word.chop! << punctuations unless punctuations.blank?
+      memo << pig_latin_word
+    end
+    foo.strip
   end
 
 end
--- test/unit/why_nots/pig_latin_test.rb
+++ test/unit/why_nots/pig_latin_test.rb
@@ -1,10 +1,23 @@
 require "test_helper"
 
-class PigLatinTest < ActiveSupport::TestCase
+class WhyNots::PigLatinTest < ActiveSupport::TestCase
 
-  context "this" do
-    should "fail" do
-      flunk "all is fine"
+  context "'hello world'" do
+    should "be translated to 'ellohay orldway'" do
+      assert_equal 'ellohay orldway', WhyNots::PigLatin.translate('hello world')
     end
   end
+
+  #context "'Hello World'" do
+  #  should "be translated to 'Ellohay Orldway'" do
+  #    assert_equal 'Ellohay Orldway', WhyNots::PigLatin.translate('Hello World')
+  #  end
+  #end
+  #
+  #context "'Hello World!!!'" do
+  #  should "be translated to 'Ellohay Orldway!!!'" do
+  #    assert_equal 'Ellohay Orldway!!!', WhyNots::PigLatin.translate('Hello World!!!')
+  #  end
+  #end
+
 end
