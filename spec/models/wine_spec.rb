e85817ec3fdcc57702fdd79fc5d62345edfa4baa
Mary Wong <pair+mwong@modcloth.com>
2013-01-19 16:50:41 -0800

    active record dynamic finders

--- spec/models/wine_spec.rb
+++ spec/models/wine_spec.rb
@@ -7,14 +7,52 @@ describe Wine do
   end
 
   describe "find non existing wine" do
-    it "should return nil" do
+    it "returns nil" do
       Wine.find_by_name("Not to be found").should be_nil
     end
   end
 
   describe "find existing wine" do
-    it "should return wine" do
+    it "returns wine" do
       Wine.find_by_name(@wine.name).should_not be_nil
+    end
+  end
+
+  describe "active record dynamic finders" do
+    describe "respond_to? find_by_varietal" do
+      it "returns true" do
+        Wine.respond_to?(:find_by_varietal).should be_true
+      end
+    end
+
+    describe "respond_to? find_all_by_varietal" do
+      it "returns true" do
+        Wine.respond_to?(:find_all_by_varietal).should be_true
+      end
+    end
+
+    describe "respond_to? find_last_by_varietal" do
+      it "returns true" do
+        Wine.respond_to?(:find_last_by_varietal).should be_true
+      end
+    end
+
+    describe "respond_to? find_or_initialize_by_varietal" do
+      it "returns true" do
+        Wine.respond_to?(:find_or_initialize_by_varietal).should be_true
+      end
+    end
+
+    describe "respond_to? find_or_create_by_varietal" do
+      it "returns true" do
+        Wine.respond_to?(:find_or_create_by_varietal).should be_true
+      end
+    end
+
+    describe "respond_to? get_by_varietal" do
+      it "returns false" do
+        Wine.respond_to?(:get_by_varietal).should be_false
+      end
     end
   end
 
