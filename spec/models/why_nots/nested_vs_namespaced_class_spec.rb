dc10f50cee0f11bcd2f68a912167b8f6005869f9
Mary Wong <pair+mwong@modcloth.com>
2013-01-25 00:11:13 -0800

    more scoping behavior for constants in nested class vs namespaced class

--- spec/models/why_nots/nested_vs_namespaced_class_spec.rb
+++ spec/models/why_nots/nested_vs_namespaced_class_spec.rb
@@ -8,6 +8,18 @@ describe WhyNots do
   it 'exposes its own FAVORITE_AUTHOR' do
     subject::FAVORITE_AUTHOR.should == "Sofi Oksanen"
   end
+
+  it 'returns its constant' do
+    subject.const_get(:FAVORITE_BOOK).should == "Purge"
+  end
+
+  it 'returns its ancestors constant' do
+    subject.const_get(:FAVORITE_YEAR).should == 2013
+  end
+
+  it 'does not returns its ancestors constant' do
+    lambda { subject.const_get(:FAVORITE_YEAR, false) }.should raise_error NameError
+  end
 end
 
 describe WhyNots::NestedClass do
@@ -19,12 +31,29 @@ describe WhyNots::NestedClass do
     subject.class::FAVORITE_COLOR.should == :white
   end
 
-  it 'has access to nested modules FAVORITE_AUTHOR constant' do
+  it 'has access to nesting modules FAVORITE_AUTHOR constant' do
     subject.class.favorite_author.should == "Sofi Oksanen"
   end
 
-  it 'has access to nested modules FAVORITE_BOOK constant' do
+  it 'has access to nesting modules FAVORITE_BOOK constant' do
     subject.class.favorite_book.should == "Purge"
+  end
+
+  it 'returns its constant' do
+    subject.class.const_get(:FAVORITE_COLOR).should == :white
+  end
+
+  it 'does not return its nesting module constant since module is not considered an ancestor' do
+    lambda { subject.class.const_get(:FAVORITE_AUTHOR) }.should raise_error NameError
+    lambda { subject.class.const_get(:FAVORITE_BOOK) }.should raise_error NameError
+  end
+
+  it 'returns the top level constant' do
+    subject.class.const_get(:FAVORITE_YEAR).should == 2013
+  end
+
+  it 'does not return the top level constant' do
+    lambda { subject.class.const_get(:FAVORITE_YEAR, false) }.should raise_error NameError
   end
 end
 
@@ -37,11 +66,24 @@ describe WhyNots::NamespacedClass do
     subject.class::FAVORITE_NUMBER.should == 777
   end
 
-  it 'does not have access to name-spaced modules FAVORITE_AUTHOR constant' do
+  it 'does not have access to namespacing modules FAVORITE_AUTHOR constant' do
     lambda { subject.class.favorite_author }.should raise_error NameError
   end
 
-  it 'does not have access to name-spaced modules FAVORITE_BOOK constant' do
+  it 'does not have access to namespacing modules FAVORITE_BOOK constant' do
     lambda { subject.class.favorite_book }.should raise_error NameError
+  end
+
+  it 'does not return its namespacing module constant since module is not considered an ancestor' do
+    lambda { subject.class.const_get(:FAVORITE_AUTHOR) }.should raise_error NameError
+    lambda { subject.class.const_get(:FAVORITE_BOOK) }.should raise_error NameError
+  end
+
+  it 'returns the top level constant' do
+    subject.class.const_get(:FAVORITE_YEAR).should == 2013
+  end
+
+  it 'does not return the top level constant' do
+    lambda { subject.class.const_get(:FAVORITE_YEAR, false) }.should raise_error NameError
   end
 end
