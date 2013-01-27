a681e52039e2998f40410ab06d0f4894594edde9
Mary Wong <pair+mwong@modcloth.com>
2013-01-26 23:38:29 -0800

    class including module

--- app/models/why_nots/my_class.rb
+++ app/models/why_nots/my_class.rb
@@ -2,6 +2,10 @@ module WhyNots
   class MyClass
     include MyModule
 
+    def initialize
+      @my_class_var = "hello"
+    end
+
     def call_debug
       debug
     end
--- spec/models/why_nots/my_class_spec.rb
+++ spec/models/why_nots/my_class_spec.rb
@@ -2,39 +2,55 @@ require 'spec_helper'
 
 describe WhyNots::MyClass do
 
-  it "calls included module's method" do
+  it "has access to included module's instance method" do
     subject.call_debug.should == WhyNots::MyClass
   end
 
-  it "calls included module's class method" do
+  it "has access to included module's class method" do
     subject.call_debug_module.should == Module
   end
 
-  it "has the correct class" do
+  it "exposes included module's instance method" do
+    subject.respond_to?(:debug).should be_true
+  end
+
+  it "is of the correct class" do
     subject.instance_of?(WhyNots::MyClass).should be_true
   end
 
-  it "has the correct class" do
+  it "its class is a Class object" do
     subject.class.instance_of?(Class).should be_true
   end
 
-  it "has the correct class" do
+  it "its class is a Class object" do
     subject.class.class.should == Class
   end
 
-  it "has the correct superclass" do
+  it "its class has the correct superclass" do
     subject.class.superclass.should == Object
   end
 
-  it "has the correct parent" do
+  it "its class has the correct parent" do
     subject.class.parent.should == WhyNots
   end
 
-  it "has the correct ancestors" do
+  it "its class has the correct ancestors" do
     subject.class.ancestors[0..2].should == [WhyNots::MyClass, WhyNots::MyModule, Object]
   end
 
-  it "has mixed in module's class method, but not the way expected !!!!!" do
+  it "its class has included module's extended module's class method, thru class's ancestors" do
     subject.class.ancestors[1].methods.include?(:other_module_name).should be_true
+  end
+
+  it "its class has included module's thru its included_modules" do
+    subject.class.included_modules.should include WhyNots::MyModule
+  end
+
+  it "its class does not have included module's extended module's thru its included_modules" do
+    subject.class.included_modules.should_not include WhyNots::MyOtherModule
+  end
+
+  it "has its own instance variables" do
+    subject.instance_variables.should == [:@my_class_var]
   end
 end
