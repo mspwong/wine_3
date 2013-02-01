aca3ac5ea6a1798e55f316ea2e3b1585b0f37517
Mary Wong <pair+mwong@modcloth.com>
2013-01-31 17:07:57 -0800

    added jasmine spec

--- spec/javascripts/why_nots_spec.js
+++ spec/javascripts/why_nots_spec.js
@@ -11,10 +11,22 @@ describe('why nots', function() {
   });
 
   describe('::showsWine', function() {
-    it('shows wine', function() {
+    it('displays wine', function() {
       $('<div id="ajax_response"></div>').appendTo($fixture);
       spyOn($.ajaxSettings, 'success');
       var json = {abc: 1, xyz: 99};
+      whyNots.showWine(json);
+
+      expect($('#ajax_response p')).toHaveHtml(JSON.stringify(json));
+      expect($.ajaxSettings.success).toHaveBeenCalled();
+    });
+  });
+
+  describe('::showsWines', function() {
+    it('displays wines', function() {
+      $('<div id="ajax_response"></div>').appendTo($fixture);
+      spyOn($.ajaxSettings, 'success');
+      var json = [{abc: 1, def: 99}, {xyz: 3, lmn: 4}];
       whyNots.showWine(json);
 
       expect($('#ajax_response p')).toHaveHtml(JSON.stringify(json));
