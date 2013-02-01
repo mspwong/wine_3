5747e764c0154bdd6db9d1904d84f0baa3453c75
Mary Wong <pair+mwong@modcloth.com>
2013-01-31 17:04:19 -0800

    added jasmine spy for existing spec

--- spec/javascripts/why_nots_spec.js
+++ spec/javascripts/why_nots_spec.js
@@ -13,11 +13,12 @@ describe('why nots', function() {
   describe('::showsWine', function() {
     it('shows wine', function() {
       $('<div id="ajax_response"></div>').appendTo($fixture);
+      spyOn($.ajaxSettings, 'success');
       var json = {abc: 1, xyz: 99};
-      var xmlHttpRequest = {status: 200};
-      whyNots.showWine(json, null, xmlHttpRequest);
+      whyNots.showWine(json);
 
       expect($('#ajax_response p')).toHaveHtml(JSON.stringify(json));
+      expect($.ajaxSettings.success).toHaveBeenCalled();
     });
   });
 
